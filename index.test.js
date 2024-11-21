2b2c33b194c7a6c4857c9c446619ea5ae3390dc5
loganlieou <logan.lieou@gmail.com>
2024-11-21 16:25:07 -0600

    jesting

--- index.test.js
+++ index.test.js
@@ -1,5 +1,5 @@
-const geo = require("./index")
+const { fromZip } = require('./index')
 
 test ("working smoke", () => {
-    expect(geo.fromZip('76020')).not.toBe(null)
+    expect(fromZip('76020')).not.toBe(null)
 })
